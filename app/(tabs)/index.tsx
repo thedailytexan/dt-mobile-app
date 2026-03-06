9900824699d5e2972509e07838f4afce32a6d730
ParasNn <40841618+ParasNn@users.noreply.github.com>
2026-03-06 00:10:26 -0600

    page clean up

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,25 +1,9 @@
-import { ScrollView, Text } from "react-native";
-import { ArticleCard } from "../../src/components/article-card";
+import { Text, View } from "react-native";
 
 export default function Home() {
   return (
-    <ScrollView contentContainerStyle={{ padding: 16, paddingTop: 60 }}>
-      <Text style={{ paddingBottom: 5, paddingLeft: 5}}>News</Text>
-      <ArticleCard category="News" index = {4} />
-      <Text style={{ paddingBottom: 5, paddingLeft: 5}}>Sports</Text>
-      <ArticleCard category="Sports" index = {1} />
-      <Text style={{ paddingBottom: 5, paddingLeft: 5}}>Life&Arts</Text>
-      <ArticleCard category="Life&Arts" />
-      <Text style={{ paddingBottom: 5, paddingLeft: 5}}>Projects</Text>
-      <ArticleCard category="Projects" />
-      <Text style={{ paddingBottom: 5, paddingLeft: 5}}>Opinion</Text>
-      <ArticleCard category="Opinion" />
-      <Text style={{ paddingBottom: 5, paddingLeft: 5}}>Multimedia</Text>
-      <ArticleCard category="Multimedia" />
-      <Text style={{ paddingBottom: 5, paddingLeft: 5}}>Texan en Español</Text>
-      <ArticleCard category="Texan en Español" />
-      <Text style={{ paddingBottom: 5, paddingLeft: 5}}>Obituaries</Text>
-      <ArticleCard category="Obituaries" />
-    </ScrollView>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Home Page</Text>
+    </View>
   );
 }
--- app/(tabs)/sports.tsx
+++ app/(tabs)/sports.tsx
@@ -1,16 +1,9 @@
 import { Text, View } from "react-native";
-import { MediumArticleCard } from "../../src/components/medium-article-card";
-import { SmallArticleCard } from "../../src/components/small-article-card";
 
 export default function Sports() {
   return (
-    <View style={{ flex: 1, padding: 16, paddingTop: 60 }}>
-      <Text style={{ fontSize: 24, fontWeight: 'bold', marginBottom: 20 }}>Sports Page</Text>
-      <View style={{ flexDirection: 'row', justifyContent: 'space-between', flexWrap: 'wrap' }}>
-        <SmallArticleCard category="Sports" index={0} />
-        <SmallArticleCard category="Sports" index={1} />
-      </View>
-      <MediumArticleCard category="Sports" index={3}/>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Sports Page</Text>
     </View>
   );
 }
