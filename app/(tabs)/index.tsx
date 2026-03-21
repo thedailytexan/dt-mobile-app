15c75a1ce1c8e05a3c187351525108c5da3f60c4
raerawr <121990754+raerawr@users.noreply.github.com>
2026-03-21 00:17:25 -0500

    Home Page UI

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,56 @@
-import { Text, View } from "react-native";
+import { Text, View, Image, FlatList } from "react-native";
+import { ArticleCard } from "../../src/components/article-card";
+import { SmallArticleCard } from "../../src/components/small-article-card";
+
 
 export default function Home() {
+  const trendingIndexes = Array.from({ length: 10 }, (_, i) => i);
+
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Home Page</Text>
+    <View>
+
+      {/* Header */}
+      <View style={{ backgroundColor: "#BF5700", height: 100, justifyContent: "flex-end", alignItems: "center" }}> 
+        <Image 
+          source={require('../../assets/images/The Daily Texan.png')}
+          style={{ width: 200, height: 27, marginBottom: 10 }}
+        />
+      </View>
+
+      {/* Top Stories */}
+      <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center", marginBottom: 10, marginTop: 25, paddingHorizontal: 16 }}>
+          <Text style={{ fontSize: 30, fontWeight: "bold" }}>Top Stories</Text>
+          <View style={{ height: 2, backgroundColor: "#000000", width: 190, marginLeft: 10 }}></View>
+      </View>
+      <FlatList
+        data={trendingIndexes}
+        showsVerticalScrollIndicator={false}
+        style={{ height: 310 }}
+        keyExtractor={(item) => item.toString()}
+        renderItem={({ item }) => (
+          <View style={{ paddingHorizontal: 16 }}>
+            <ArticleCard index={item} />
+          </View>
+        )}
+      />
+
+      {/* Latest */}
+      <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center", marginBottom: 10, marginTop: 25, paddingHorizontal: 16 }}>
+        <Text style={{ fontSize: 30, fontWeight: "bold" }}>Latest</Text>
+        <View style={{ height: 2, backgroundColor: "#000000", width: 250, marginLeft: 10 }}></View>
+      </View>
+      <FlatList
+        data={trendingIndexes}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => item.toString()}
+        renderItem={({ item }) => (
+          <View style={{ marginLeft: 13 }}>
+            <SmallArticleCard index={item} />
+          </View>
+        )}
+      />
+
     </View>
   );
 }
