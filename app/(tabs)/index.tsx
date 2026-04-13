27a63a97369cf833d6d7da605d46d59af64e8ca7
raerawr <121990754+raerawr@users.noreply.github.com>
2026-04-13 18:44:08 -0500

    profile ui complete and fixed home page ui with imported font

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,19 @@
-import { Text, View, Image, FlatList } from "react-native";
+import { Text, View, Image, FlatList, Dimensions } from "react-native";
 import { ArticleCard } from "../../src/components/article-card";
 import { SmallArticleCard } from "../../src/components/small-article-card";
+import { useFonts } from 'expo-font';
 
 
 export default function Home() {
+  const screenWidth = Dimensions.get("window").width;
   const trendingIndexes = Array.from({ length: 10 }, (_, i) => i);
+  const [loaded] = useFonts({
+    LibreBaskerville: require('../../assets/fonts/Libre-Baskerville.ttf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
 
   return (
     <View>
@@ -19,13 +28,13 @@ export default function Home() {
 
       {/* Top Stories */}
       <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center", marginBottom: 10, marginTop: 25, paddingHorizontal: 16 }}>
-          <Text style={{ fontSize: 30, fontWeight: "bold" }}>Top Stories</Text>
-          <View style={{ height: 2, backgroundColor: "#000000", width: 190, marginLeft: 10 }}></View>
+          <Text style={{ fontSize: 27, fontFamily: 'LibreBaskerville' }}>TOP STORIES</Text>
+          <View style={{ height: 2.3, backgroundColor: "#000000", width: screenWidth * 0.35, marginLeft: 10 }}></View>
       </View>
       <FlatList
         data={trendingIndexes}
         showsVerticalScrollIndicator={false}
-        style={{ height: 310 }}
+        style={{ height: 290 }}
         keyExtractor={(item) => item.toString()}
         renderItem={({ item }) => (
           <View style={{ paddingHorizontal: 16 }}>
@@ -36,8 +45,8 @@ export default function Home() {
 
       {/* Latest */}
       <View style={{ flexDirection: "row", justifyContent: "center", alignItems: "center", marginBottom: 10, marginTop: 25, paddingHorizontal: 16 }}>
-        <Text style={{ fontSize: 30, fontWeight: "bold" }}>Latest</Text>
-        <View style={{ height: 2, backgroundColor: "#000000", width: 250, marginLeft: 10 }}></View>
+        <Text style={{ fontSize: 25, fontFamily: 'LibreBaskerville' }}>LATEST</Text>
+        <View style={{ height: 2.3, backgroundColor: "#000000", width: screenWidth * 0.6, marginLeft: 10, paddingHorizontal: 16 }}></View>
       </View>
       <FlatList
         data={trendingIndexes}
@@ -50,7 +59,6 @@ export default function Home() {
           </View>
         )}
       />
-
     </View>
   );
 }
